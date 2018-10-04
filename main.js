c00ad90302a259422d5eb478a8043fbb74b8cfad
Seth J. Freeman <cyanidecylinder@gmail.com>
2018-10-04 18:15:38 -0400

    added comments

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function convert() {
-  var output = document.getElementById("output");
-  var input = document.getElementById("input").value;
-  output.value = "";
-  for (var i = 0; i < input.length; i++) {
-      output.value += input[i].charCodeAt(0).toString(2) + " ";
+  var output = document.getElementById("output"); // Sets variable to output element
+  var input = document.getElementById("input").value; // Gets input value
+
+  output.value = ""; // Resets output value before for loop does its thing
+
+  for (var i = 0; i < input.length; i++) { // loops to the length of the input element string
+      output.value += input[i].charCodeAt(0).toString(2) + " "; // output value += current input location . character code . converts to binary value + addes space between each character binary value
   }
 }
